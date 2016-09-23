28286cdd0a0b6ce38ef70ff33d759b9a0c577631
Alex Gray <alex@mrgray.com>
2016-09-23 12:37:59 -0400

    made automated readme use mos instead

--- example.js
+++ example.js
@@ -1,31 +1,13 @@
 
-testModule = function(mod){ console.log(mod.isArray([])); }
-
-/* creates a global 'need' function once required */
+//! creates a global 'need' function once required
 require('./')
-/* then, you can "need" a module, by string name */
+//! then, you can "need" a module, by string name
 need("underscore")
-testModule(underscore)
 
-/* also, returns a local function, with same functionality as the global "need" */
+//! also, returns a local function, with same functionality as the global "need"
 n = require("./")
-/* require with a hash, mapping your imported module (value) to any name (key) */
+//! require with a hash, mapping your imported module (value) to any name (key)
 n({_:"underscore"})
-testModule(_)
 
-/* use your array, string or hash when requring, all your imports.. on one line! */
+//! use your array, string or hash when requring, all your imports.. on one line!
 require('./')(["underscore"])
-testModule(underscore)
-
-/* RESULTS:
-
-[need]	Requiring underscore, as underscore.
-true
-arguments are [object Object]
-[need]	Requiring underscore, as _.
-true
-arguments are underscore
-[need]	Requiring underscore, as underscore.
-true
-
-*/
